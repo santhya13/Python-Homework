36265ebcfc354cc0f1369852263cacbaaaa61b87
sselvakularanjan <selvakularanjan@gmail.com>
2019-12-21 16:49:10 -0600

    Made adjustments to the variables; added functions

--- PyBank/.ipynb_checkpoints/main-checkpoint.py
+++ PyBank/.ipynb_checkpoints/main-checkpoint.py
@@ -1,20 +1,39 @@
-#Import Libraries
+# Import libraries
 from pathlib import Path
 import csv
 
-#Set File Path
+# Set path for file
 csvpath = Path("Resources/budget_data.csv")
 
-#Initialize Variables
-mouth_count = 0
+#Declare Variables
+months = 0 
 net_revenue = 0
-average_change = 0
+prior_value = 0
+average_change = []
+greatest_increase = 0
+greatest_increase_date = ""
+greatest_decrease = 0
+greatest_decrease_date = ""
 
+# Open and read the CSV
 with open(csvpath, "r") as csvfile:
+    print(type(csvfile))
     csvreader = csv.reader(csvfile, delimiter=',')
-
-    
-    
+    print(type(csvreader))
+    header = next(csvfile)
+    print(f"Header: {header}")
+    row_num += 0
+      
+    for row in csvreader:
+        print(row)
+    # Average Change in Months
+        difference = prior_value - int(row[1])
+        average.append(difference)
+        prior_value = int(row[1])
+        average_change = sum(average) / len(average)
+    # Find the Net Revenue    
+        net_revenue = int(row[1])
+        
 #Print Resulting Analysis
 print("Financial Analysis")
 print("----------------------------------------")

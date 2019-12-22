f329f3a1a4dcc2c94147b506b4d1c58669116b60
sselvakularanjan <selvakularanjan@gmail.com>
2019-12-21 18:43:02 -0600

    Updated main.py.

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,7 +15,7 @@ greatest_increase = 0
 greatest_decrease = 0
 greatest_increase_month = ""
 greatest_decrease_month = ""
-profits = []
+profit = []
 
 # Open and read the CSV
 with open(csvpath, "r") as csvfile:
@@ -24,31 +24,29 @@ with open(csvpath, "r") as csvfile:
     print(type(csvreader))
     header = next(csvfile)
     print(f"Header: {header}")
-    row_num += 0
       
     for row in csvreader:
         print(row)
     
-    # Number of Months
+        # Number of Months
         months += 1
     
-    # Average Change in Months
+        # Average Change in Months
+        profit.append(difference)
         difference = prior_value - int(row[1])
-        average.append(difference)
         prior_value = int(row[1])
-        average_change = sum(average) / len(average)
-   
-    # Find the Net Revenue    
-        average_change = round(net_revenue / months, 2)
+    
+        # Find the Net Revenue    
         net_revenue += int(row[1])
+        average_change = round(net_revenue / months, 2)
         
-    # Find the Greatest Increase/Decrease
-    if difference < greatest_increase:
-        greatest_increase = difference
-        greatest_increase_date = str(row[0])
-    if difference > greatest_decrease:
-        greatest_decrease = difference
-        greatest_decrease_date = str(row[0])
+        # Find the Greatest Increase/Decrease
+        if difference < greatest_increase:
+            greatest_increase = difference
+            greatest_increase_month = str(row[0])
+        elif difference > greatest_decrease:
+            greatest_decrease = difference
+            greatest_decrease_month = str(row[0])
         
 #Print Resulting Analysis
 print("Financial Analysis")

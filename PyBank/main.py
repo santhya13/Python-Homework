c96c3d7f57b1a706e7b9b06ee95761eeb6d74468
sselvakularanjan <selvakularanjan@gmail.com>
2020-01-16 05:20:49 -0600

    Redid assignment in order to fix errors.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,58 +1,61 @@
-# Import libraries
+#Import Library
 from pathlib import Path
 import csv
+import os
 
 # Set path for file
 csvpath = Path("Resources/budget_data.csv")
 
-#Declare Variables
-months = 0 
-net_revenue = 0
-difference = 0
-prior_value = 0
-average_change = 0
-greatest_increase = 0
-greatest_decrease = 0
+#Create Lists
+net_revenue = []
+monthly_changes = []
 greatest_increase_month = ""
 greatest_decrease_month = ""
-profit = []
 
-# Open and read the CSV
+#Initialize the Variables
+months = 0
+initial_value = 0
+total_revenue = 0
+profit_changes = 0
+greatest_increase = 0
+greatest_decrease = 0
+
+# Open and Read the CSV
 with open(csvpath, "r") as csvfile:
-    print(type(csvfile))
     csvreader = csv.reader(csvfile, delimiter=',')
-    print(type(csvreader))
-    header = next(csvfile)
-    print(f"Header: {header}")
-      
+    header = next(csvreader)
+    
     for row in csvreader:
-        print(row)
-    
-        # Number of Months
+        
+        #Number of Months
         months += 1
-    
-        # Average Change in Months
-        profit.append(difference)
-        difference = prior_value - int(row[1])
-        prior_value = int(row[1])
-    
-        # Find the Net Revenue    
-        net_revenue += int(row[1])
-        average_change = round(net_revenue / months, 2)
+        
+        net_revenue.append(row[1])
+        
+        # Average Change in Net Revenue
+        total_revenue = total_revenue + int(row[1])
+        final_value = int(row[1])
+        difference = final_value - initial_value
+        monthly_changes.append(difference)
+        profit_changes = profit_changes + difference
+        initial_value = final_value
+        
+        # Average Change in Net Revenue Rounded
+        average_change = round(profit_changes / months, 2)
         
         # Find the Greatest Increase/Decrease
-        if difference < greatest_increase:
+        if difference > greatest_increase:
             greatest_increase = difference
             greatest_increase_month = str(row[0])
-        elif difference > greatest_decrease:
+        if difference < greatest_decrease:
             greatest_decrease = difference
             greatest_decrease_month = str(row[0])
-        
+            
 #Print Resulting Analysis
 print("Financial Analysis")
 print("----------------------------------------")
 print(f"Total Months: {months}")
-print(f"Total Revenue: ${net_revenue}")
+print(f"Total Revenue: ${total_revenue}")
 print(f"Average Revenue Change: ${average_change}")
-print(f"Greatest Increase in Revenue: {max_month} (${max_change})")
-print(f"Greatest Decrease in Revenue: {min_month} (${min_change})")
+print(f"Greatest Increase in Revenue: {greatest_increase_month} (${greatest_increase})")
+print(f"Greatest Decrease in Revenue: {greatest_decrease_month} (${greatest_decrease})")
